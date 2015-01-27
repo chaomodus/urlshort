124c167bb5d8f08d695d7eb88211220faf26d9a4
Cas Rusnov <rusnovn@gmail.com>
2015-01-27 04:18:38 -0800

    Remove some erroneous files.

--- database/createdb.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE urlshort WITH OWNER urlshort;
--- service/templates/createdb.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER urlshort WITH PASSWORD 'urlshort';
-CREATE DATABASE urlshort;
-GRANT ALL PRIVILEGES ON DATABASE urlshort TO urlshort;
